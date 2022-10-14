9576f9c0c2afbde07d698f5b6dd03b3805d7e972
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-13 21:28:19 -0400

    finished translating military time and using css classes to represent time of day with colors

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 //step 1: put current date using moment()
 //step 2: compare the current hour with time-blocks
+//step 2.5: translate time from military time
 //step 3: use css classes for present, past, and future
 //step 4: grab the text area value and store it in local storage
 
-var currenthour= moment().hour()
+var timeBlocksArr = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+var currenthour = moment().hour()
+
+var currentDay = $("#currentDay")
+
+var systemTime = moment().format("dddd, MMMM Do")
+currentDay.text(systemTime)
+
+function displayColor(){
+    for(var i=0; i < timeBlocksArr.length;i++){
+        var currentTextEl = $("#" +timeBlocksArr[i])
+
+        if(currenthour>timeBlocksArr[i]){
+           currentTextEl.addClass("past")
+        }
+        else if (currenthour === timeBlocksArr[i]) {
+       currentTextEl.addClass("present")     
+        }
+        else if(currenthour < timeBlocksArr[i]){
+            currentTextEl.addClass("future")
+        }
+    }
+}
+
+displayColor()
