3c51d71656b7dc87978fdfbe3d7ffb9ed2f63a2c
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-13 21:06:48 -0400

    added pseudocode to js

---
+++ script.js
@@ -0,0 +1,6 @@
+//step 1: put current date using moment()
+//step 2: compare the current hour with time-blocks
+//step 3: use css classes for present, past, and future
+//step 4: grab the text area value and store it in local storage
+
+var currenthour= moment().hour()
